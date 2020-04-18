595543459fcedc9dd6bf146471593e9d997162e0
Michael Kim <m.kim4247@gmail.com>
2020-04-18 17:11:29 -0400

    added namegen fxn and env file

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-console.log('fuku island')
+console.log('fuku island online!')
 
 const fetch = require('node-fetch')
+const fs = require('fs');
+const jsonfile = require('jsonfile');
+const dotenv = require('dotenv').config();
+const token = process.env.TOKEN
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'NzAwNDE0ODEzNDI3MjA0MTU2.XpineA.PHhvhaF7yIV1ocId1B5hoUMYAnQ'
 const channelId = '700421543041171568'
+
+let stats = {};
+if(fs.existsSync('stats.json')){
+  stats = jsonfile.readFileSync('stats.json');
+}
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -17,8 +26,12 @@ client.on("guildMemberAdd", member => {
 });
 
 client.on('message', msg => {
-  if(msg.author.bot) return;
-  if(msg.content === '/dad') {
+  if(msg.author.bot){
+    return
+  }
+
+  let [command, ...parts] = msg.content.split(' ');
+  if(command === '!dad'){
     fetch('https://icanhazdadjoke.com/', {
       method: "GET",
       headers: {
@@ -28,9 +41,46 @@ client.on('message', msg => {
     .then(res => res.json())
     .then(data => msg.reply(data.joke))
   }
+  if(command === '!hello'){
+    msg.reply(`hi c:`);
+  };
+
+  if(command === '!namegen'){
+    let rnd = Math.random();
+    //shuffle the array of names
+    for (let i = parts.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+      [parts[i], parts[j]] = [parts[j], parts[i]];
+    }
+    let str = ''
+    for(let k = 0; k < parts.length; k++){
+      str += `\n${k + 1}. ${parts[k]}`
+    }
+    msg.channel.send(`Name Generator: ${str}`)
+  }
+
+  if(msg.guild.id in stats === false){
+    stats[msg.guild.id] = {};
+  }
+
+  const guildStats = stats[msg.guild.id];
+  if(msg.author.id in guildStats === false) {
+    guildStats[msg.author.id] = {
+      level: 0,
+      msgCount: 1
+    };
+  }
+  else {
+    guildStats[msg.author.id].msgCount++
+  };
+
+  const userStats = guildStats[msg.author.id];
+  if(userStats.msgCount % 3 === 0){
+    userStats.level++;
+    msg.channel.send(`${msg.author.toString()} has reached level ${userStats.level}!`);
+    jsonfile.writeFileSync('stats.json', stats);
+  }
+
 });
-
-
-
 
 client.login(token);
