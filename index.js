bffe7c60771e0a19197818ebdf1f1cd509c0bfdf
Michael Kim <m.kim4247@gmail.com>
2020-05-11 20:12:28 -0400

    working w DialogFlow

--- index.js
+++ index.js
@@ -30,78 +30,78 @@ client.on('ready', () => {
     });
     task.start();
 });
-
-const sessionId = uuid.v4();
-const languageCode = 'en';
-const queries = [
-  'Reserve a meeting room in Toronto office, there will be 5 of us',
-  'Next monday at 3pm for 1 hour, please', // Tell the bot when the meeting is taking place
-  'B'  // Rooms are defined on the Dialogflow agent, default options are A, B, or C
-]
-
-// Instantiates a session client
-const sessionClient = new dialogflow.SessionsClient();
-
-async function detectIntent(
-  projectId,
-  sessionId,
-  query,
-  contexts,
-  languageCode
-) {
-  // The path to identify the agent that owns the created intent.
-  const sessionPath = sessionClient.sessionPath(
-    projectId,
-    sessionId
-  );
-
-  // The text query request.
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      text: {
-        text: query,
-        languageCode: languageCode,
-      },
-    },
-  };
-
-  if (contexts && contexts.length > 0) {
-    request.queryParams = {
-      contexts: contexts,
-    };
-  }
-
-  const responses = await sessionClient.detectIntent(request);
-  return responses[0];
-}
-
-async function executeQueries(projectId, sessionId, queries, languageCode) {
-  // Keeping the context across queries let's us simulate an ongoing conversation with the bot
-  let context;
-  let intentResponse;
-  for (const query of queries) {
-    try {
-      console.log(`Sending Query: ${query}`);
-      intentResponse = await detectIntent(
-        projectId,
-        sessionId,
-        query,
-        context,
-        languageCode
-      );
-      console.log('Detected intent');
-      console.log(
-        `Fulfillment Text: ${intentResponse.queryResult.fulfillmentText}`
-      );
-      // Use the context from this response for next queries
-      context = intentResponse.queryResult.outputContexts;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-}
-executeQueries(projectId, sessionId, queries, languageCode);
+//
+// const sessionId = uuid.v4();
+// const languageCode = 'en';
+// const queries = [
+//   'Reserve a meeting room in Toronto office, there will be 5 of us',
+//   'Next monday at 3pm for 1 hour, please', // Tell the bot when the meeting is taking place
+//   'B'  // Rooms are defined on the Dialogflow agent, default options are A, B, or C
+// ]
+//
+// // Instantiates a session client
+// const sessionClient = new dialogflow.SessionsClient();
+//
+// async function detectIntent(
+//   projectId,
+//   sessionId,
+//   query,
+//   contexts,
+//   languageCode
+// ) {
+//   // The path to identify the agent that owns the created intent.
+//   const sessionPath = sessionClient.sessionPath(
+//     projectId,
+//     sessionId
+//   );
+//
+//   // The text query request.
+//   const request = {
+//     session: sessionPath,
+//     queryInput: {
+//       text: {
+//         text: query,
+//         languageCode: languageCode,
+//       },
+//     },
+//   };
+//
+//   if (contexts && contexts.length > 0) {
+//     request.queryParams = {
+//       contexts: contexts,
+//     };
+//   }
+//
+//   const responses = await sessionClient.detectIntent(request);
+//   return responses[0];
+// }
+//
+// async function executeQueries(projectId, sessionId, queries, languageCode) {
+//   // Keeping the context across queries let's us simulate an ongoing conversation with the bot
+//   let context;
+//   let intentResponse;
+//   for (const query of queries) {
+//     try {
+//       console.log(`Sending Query: ${query}`);
+//       intentResponse = await detectIntent(
+//         projectId,
+//         sessionId,
+//         query,
+//         context,
+//         languageCode
+//       );
+//       console.log('Detected intent');
+//       console.log(
+//         `Fulfillment Text: ${intentResponse.queryResult.fulfillmentText}`
+//       );
+//       // Use the context from this response for next queries
+//       context = intentResponse.queryResult.outputContexts;
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+// }
+// executeQueries(projectId, sessionId, queries, languageCode);
 
 
 
